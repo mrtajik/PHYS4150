26d9e404623a7b4f12a093f5ec69df5a5c2dea5b
mubinjon <mubs@mubinjons-Air.home>
2019-09-10 21:05:32 -0400

    Exercise 2.1

---
+++ ex2.1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+g=9.8
+s=float(input("Enter hight of the building: "))
+s1=np.sqrt((s)/(.5*g))
+print(s1)
+
+
+# In[2]:
+
+
+import numpy as np
+g=9.8
+s=100
+s1=np.sqrt((s)/(.5*g))
+print(s1)
+
+
+# In[ ]:
+
+
+
+

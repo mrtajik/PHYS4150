c5230659189bf51d5d42cf49f1a7396bbcb50514
mrtajik <satymov@outlook.com>
2019-09-26 02:01:14 -0400

    Update

--- ex3.2.py
+++ ex3.2.py
@@ -27,7 +27,7 @@ plt.plot(x,y)
 plt.show()
 
 #Part b:
-a=np.linspace(0,10*np.pi,num=1000) #More set of numbers creates smoother graph
+a=np.linspace(0,10*np.pi,num=10000) #More set of numbers creates smoother graph
 r=a**2
 x1=r*np.cos(a)
 y1=r*np.sin(a)
@@ -35,7 +35,7 @@ plt.plot(x1,y1)
 plt.show()
 
 #Part c:
-a=np.linspace(0,10*np.pi,num=100000)
+a=np.linspace(0,24*np.pi,num=100000)
 r=(np.exp(np.cos(a)))-2*np.cos(4*a)+(np.sin(a/12))**5
 x2=r*np.cos(a)
 y2=r*np.sin(a)

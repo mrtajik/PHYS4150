46a574e4c74f681f70dd01a9b42fb3e7da2e1c8c
mrtajik <satymov@outlook.com>
2019-09-26 01:56:26 -0400

    added comments

--- ex2.1.py
+++ ex2.1.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
+"""
+Mubinjon Satymov
 
-# In[1]:
+Exercise 2.1: 
+Another ball dropped from a tower a ball is dropped from a tower of height h 
+with initial velocity zero. Write a program that asks the user to enter the 
+height in meters of the tower and then calculates and prints the time the ball 
+takes until it hits the ground, ignoring air resistance. Use your program to 
+calculate the time for a ball dropped from a 100 m high tower.
 
-
+Solution:
+General formula to calculate:
+    s=0.5*g*(t^2)
+    To calculate time we use t=((2*s)/(g))^(0.5)
+"""
 import numpy as np
-g=9.8
-s=float(input("Enter hight of the building: "))
-s1=np.sqrt((s)/(.5*g))
-print(s1)
-
-
-# In[2]:
-
-
-import numpy as np
-g=9.8
-s=100
-s1=np.sqrt((s)/(.5*g))
-print(s1)
-
-
-# In[ ]:
-
-
-
-
+import scipy.constants as const
+import astropy.units as unit
+g=const.g                           #declaring constant "g" in SI
+s=float(input("Enter hight of the building: "))                    
+t=(np.sqrt((2*s)/(g)))              #Calculating value of "t"
+t1=t*unit.s                         #Adding SI unit(second) to value of t
+print("It takes {:.2f} until the ball hits the ground".format(t1))
+#For H=100 we just enter value of H to our program
--- ex2.4.py
+++ ex2.4.py
@@ -1,31 +1,38 @@
-#!/usr/bin/env python
-# coding: utf-8
+"""
+Mubinjon Satymov
 
-# In[ ]:
+Exercise 2.4:
+A spaceship travels from Earth in a straight line at relativistic speed v to 
+another planet x light years away. Write a program to ask the user for the 
+value of x and the speed v as a fraction of the speed of light c, then print 
+out the time in years that the spaceship takes to reach its destination (a) in 
+the rest frame of an observer on Earth and (b) as perceived by a passenger on 
+board the ship. Use your program to calculate the answers for a planet 10 light
+years away with v = 0.99c..
 
+Solution:
+Part a:For observer on Earth we use:
+    t=x/v; where 
+    x-How far planet X  away in light years;
+    v=Relativistic speed of a spaceship;
 
+Part b: For a passenger on board the ship we use:
+    t0=(t)*((1-v^2)^(0.5)); where 
+    t-time dialation; 
+    t=x/v;  
+    x=10; 
+    v=0.99;
+"""
 #Part a
 import numpy as np
 x=float(input("How far planet X  away in light years= "))
-v=float(input("Relativistic speed of a spaceship V= "))
-t0=x*v
-print("For observer on Earth spaceship would reach planet X in ",t0,"light years")
+v=float(input("Relativistic speed of a spaceship v= "))
+t=x/v
+print("For observer on Earth spaceship would reach planet X in {:.2f} light years".format(t))
 
-
-# In[13]:
-
-
-#Part 
-import numpy as np
-x=10 #distance
+#Part b 
+x=10  #distance
 v=.99 #speed
-t0=x*v
-t=(t0)/(np.sqrt(1-(v**2)))
-print("Passenger on board ship would reach planet X in ",t,"light years")
-
-
-# In[ ]:
-
-
-
-
+t=x/v
+t0=t*(np.sqrt(1-(v**2)))
+print("Passenger on board ship would reach planet X in {:.2f} light years".format(t0))
